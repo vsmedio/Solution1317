7ee24be4364650ec7fb118cf70c0a04e53de4673
Arvis Abromovics <info@vsmedio.lv>
2018-01-10 20:26:24 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,65 @@
+/* 
+The weather is fine
+1. В классе Today реализовать интерфейс Weather.
+2. Подумай, как связан параметр type с методом getWeatherType().
+3. Интерфейсы Weather и WeatherType уже реализованы в отдельных файлах.
+
+
+Требования:
+1. Интерфейс Weather должен быть реализован в классе Today.
+2. В классе Today должен быть реализован метод getWeatherType объявленный в интерфейсе Weather.
+3. Тип возвращаемого значения метода getWeatherType должен быть String.
+4. Метод getWeatherType должен возвращать значение переменной type.
+
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Today(WeatherType.CLOUDY));
+        System.out.println(new Today(WeatherType.FOGGY));
+        System.out.println(new Today(WeatherType.FROZEN));
+    }
+
+    static class Today {
+        private String type;
+
+        Today(String type) {
+            this.type = type;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s for today", this.getWeatherType());
+        }
+    }
+}
+*/
+
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Today(WeatherType.CLOUDY));
+        System.out.println(new Today(WeatherType.FOGGY));
+        System.out.println(new Today(WeatherType.FROZEN));
+    }
+
+
+    static class Today implements Weather, WeatherType{
+        private String type;
+
+        Today(String type) {
+            this.type = type;
+        }
+
+
+
+        @Override
+        public String toString() {
+            return String.format("%s for today", this.getWeatherType());
+        }
+
+        @Override
+        public String getWeatherType() {
+            return this.type;
+        }
+    }
+}
